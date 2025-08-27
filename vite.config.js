f22c326ffa87e28e3e8514da5cc5828a5f212a73
skalaliya <skalaliya@gmail.com>
2025-08-28 00:06:02 +0200

    bug fix in css

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
+// vite.config.mjs
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(), react()],
+  plugins: [react()],
 })
